5189bd07b08c27fdca307dc8896a2335ac758b50
hrsk <hrsk.yegor@gmail.com>
2023-06-27 20:14:24 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Header} from "./components/header/Header";
+import {Footer} from "./components/footer/Footer";
 
 export const App = () => {
     return (
         <div className="App">
             <Header/>
+            <Footer/>
         </div>
     );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import style from './Footer.module.css';
+
+export const Footer = () => {
+    return (
+        <div className={style.container}>
+            <div className={style.text}>Copyright © 2021 All rights reserved.</div>
+        </div>
+    );
+}
