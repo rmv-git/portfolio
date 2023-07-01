64dfcf3562eff10ce213dae00ffed2b84c73da23
hrsk <hrsk.yegor@gmail.com>
2023-07-01 18:24:05 +0300

    programming language icons

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,11 @@
 import React from "react";
 import style from './Skills.module.css';
 import {Header} from "../../common/header/Header";
+import html_icon from './../../assets/images/programming_language_images/html5_icon.png';
+import js_icon from './../../assets/images/programming_language_images/javascript_language_icon.png';
+import react_icon from './../../assets/images/programming_language_images/react_react_native_icon.png';
+import ts_icon from './../../assets/images/programming_language_images/typescript.png';
+import express_js from './../../assets/images/programming_language_images/express_javascript.png';
 
 export const Skills = () => {
     return (
@@ -12,7 +17,7 @@ export const Skills = () => {
                 <div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
 
-                        <img className={style.imageIcon} alt={'html_icon'}/>
+                        <img className={style.imageIcon} src={html_icon} alt={'html_icon'}/>
                     </div>
                     <div className={style.contentDescription}>
                         <h3 className={style.descriptionTitle}>HTML/CSS</h3>
@@ -22,7 +27,7 @@ export const Skills = () => {
             </span>
                     </div>
                 </div>
-                <div className={style.skillWrapper}>
+                {/*<div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
                         <img className={style.imageIcon} alt={'web_design'}/>
                     </div>
@@ -33,10 +38,10 @@ export const Skills = () => {
                             eum alias id, repellendus magni, quas.
             </span>
                     </div>
-                </div>
+                </div>*/}
                 <div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
-                        <img className={style.imageIcon} alt={'js_icon'}/>
+                        <img className={style.imageIcon} src={js_icon} alt={'js_icon'}/>
                     </div>
                     <div className={style.contentDescription}>
                         <h3 className={style.descriptionTitle}>Javasript</h3>
@@ -48,7 +53,7 @@ export const Skills = () => {
                 </div>
                 <div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
-                        <img className={style.imageIcon} alt={'react_icon'}/>
+                        <img className={style.imageIcon} src={react_icon} alt={'react_icon'}/>
                         <div className={style.contentDescription}>
                             <h3 className={style.descriptionTitle}>React JS</h3>
                             <span className={style.description}>
@@ -61,7 +66,7 @@ export const Skills = () => {
                 </div>
                 <div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
-                        <img className={style.imageIcon} alt={'ts_icon'}/>
+                        <img className={style.imageIcon} src={ts_icon} alt={'ts_icon'}/>
                     </div>
                     <div className={style.contentDescription}>
                         <h3 className={style.descriptionTitle}>TypeScript</h3>
@@ -73,7 +78,7 @@ export const Skills = () => {
                 </div>
                 <div className={style.skillWrapper}>
                     <div className={style.contentSkill}>
-                        <img className={style.imageIcon} alt={'express_js'}/>
+                        <img className={style.imageIcon} src={express_js} alt={'express_js'}/>
                     </div>
                     <div className={style.contentDescription}>
                         <h3 className={style.descriptionTitle}>Express.js</h3>
